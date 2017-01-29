d61970035fc6e4f4bbe5c2211fd1ea876933c90e
BorisLS <boris.luetke.schelhowe@gmail.com>
2017-01-29 13:41:55 +0100

    Rebuild wapp_import with separate import routine for iOS and android

--- R/import.R
+++ R/import.R
@@ -1,20 +1,21 @@
 #' Imports an exported WhatsApp chat history
 #'
 #' @param history a file path (txt-file) that contains the exported WhatsApp history
-#' @param language is necessary to identify the type of the message. Note: Currently only the German language ('de') is supported
+#' @param os the operating system of the smartphone 'iOS' or 'android'
+#' @param language Note: Currently only the German language ('de') is supported
 #' @param techuser a boolean variable. If WhatsApp TechUser-Posts should be part of the dataframe or not
 #' @return a dataframe with the imported and processed WhatsApp history
 #' @export
 #' @import dplyr
-wapp_import <- function(history, language = "de", techuser = FALSE){
-  #' @import dplyr
+wapp_import <- function(history,
+                        os,
+                        language = "de",
+                        techuser = FALSE){
   # import data ---------------------------
-  # read in the chat export file
-  # each line ist separated with \n
-
   chat <- readr::read_lines(history, locale = readr::locale(encoding = "UTF-8"))
   rawdata.rows <- length(chat)
 
+  # initialise result dataframe ---------------------------
   chat.df <- data.frame(moment = rep(as.POSIXct("16.1.2017 07:04", format="%d.%m.%y, %H:%M"),
                                      rawdata.rows),
                         day = rep("t.na", rawdata.rows),
@@ -29,53 +30,68 @@ wapp_import <- function(history, language = "de", techuser = FALSE){
     raw.data <- as.character(chat[i])
     chat.df[i, "rawdata"] <- raw.data
 
-    # estimate if the line contains all information
-    third.pos <- stringr::str_sub(raw.data, start = 3, end = 3)
-    fifth.pos <- stringr::str_sub(raw.data, start = 6, end = 6)
-    sep.pos <- length(unlist(stringr::str_locate_all(pattern =":", raw.data)))
+    if(os == "android"){
+      raw.time  <- stringr::str_split_fixed(raw.data, " - ", n=2)[1]
+      raw.data1 <- stringr::str_split_fixed(raw.data, " - ", n=2)[2]
 
-    if(third.pos == "." && fifth.pos == "." && sep.pos >= 4){
-      # full information in line available
-      chat.df[i, "moment"] <- as.POSIXct(stringr::str_sub(raw.data, start = 1, end = 15),
-                                         format="%d.%m.%y, %H:%M")
-      chat.df[i, "day"]   <- stringr::str_sub(raw.data, start = 1, end = 8)
+    } else if(os == "iOS"){
+      raw.time  <- stringr::str_split_fixed(raw.data, ": ", n=2)[1]
+      raw.data1 <- stringr::str_split_fixed(raw.data, ": ", n=2)[2]
+    }
 
-      raw.data2 <- stringr::str_sub(raw.data, start = 19, end = nchar(raw.data))
-      raw.data3 <- stringr::str_split_fixed(raw.data2, ":", n=2)
-      chat.df[i, "author"] <- raw.data3[1]
-      chat.df[i, "content"]      <- stringr::str_trim(raw.data3[2], side = "left")
-
-    } else if(third.pos == "." && fifth.pos == "." && sep.pos < 4){
-      # technical information in line (e.g. add members, change chat name)
-      chat.df[i, "moment"] <- as.POSIXct(stringr::str_sub(raw.data, start = 1, end = 15),
-                                         format="%d.%m.%y, %H:%M")
-      chat.df[i, "day"]   <- stringr::str_sub(raw.data, start = 1, end = 8)
-
-      raw.data2 <- stringr::str_sub(raw.data, start = 19, end = nchar(raw.data))
-      chat.df[i, "author"] <- "TechUser"
-      chat.df[i, "content"]      <- stringr::str_trim(raw.data2, side = "left")
-
+    # is raw.time a date -> depend on language -> define separat function
+    raw.time.3 <- stringr::str_sub(raw.time, 3, 3)
+    raw.time.6 <- stringr::str_sub(raw.time, 6, 6)
+    raw.time.9 <- stringr::str_sub(raw.time, 9, 9)
+    if(raw.time.3 == "." && raw.time.6 == "." && raw.time.9 == ","){
+      is.time <- TRUE
     } else{
-      # no complete Information in line (user information is missing)
+      is.time <- FALSE
+    }
+
+    if(is.time == FALSE){
+      # no complete Information in line (user information and time is missing)
       chat.df[i, "moment"]  <- chat.df[i-1, "moment"]
       chat.df[i, "day"]     <- chat.df[i-1, "day"]
       chat.df[i, "author"]  <- chat.df[i-1, "author"]
       chat.df[i, "content"] <- stringr::str_trim(raw.data, side = "left")
+
+    } else{
+      chat.df[i, "moment"]  <- as.POSIXct(raw.time,
+                                          format="%d.%m.%y, %H:%M")
+      chat.df[i, "day"]     <- stringr::str_sub(raw.time,
+                                                start = 1, end = 8)
+
+      raw.author <- stringr::str_split_fixed(raw.data1, ": ", n = 2)[1]
+      raw.content <- stringr::str_split_fixed(raw.data1, ": ", n = 2)[2]
+
+      if(raw.author != "" && raw.content !=""){
+        # complete information in line
+        chat.df[i, "author"]  <- raw.author
+        chat.df[i, "content"] <- raw.content
+
+      } else{
+        # technical information
+        chat.df[i, "author"]  <- "TechUser"
+        chat.df[i, "content"] <- stringr::str_trim(raw.data1,
+                                                   side = "left")
+      }
     }
   }
-
-
   # data transformation  ---------------------------
   chat.df$day <- as.Date(chat.df$day, "%d.%m.%y")
   chat.df$message.length = nchar(chat.df$content)
 
   # identify message type
-  if(language == "de"){
-    chat.df$type <- ifelse(chat.df$content == "<Medien weggelassen>",
-                           "media", "message")
-  } else(
-    chat.df$type <- "unkown language"
-  )
+  first.char <- stringr::str_sub(chat.df$content,
+                                 start = 1,
+                                 end = 1)
+  last.char  <- stringr::str_sub(chat.df$content,
+                                 start = chat.df$message.length,
+                                 end   = chat.df$message.length)
+
+  chat.df$type <- ifelse(first.char == "<" & last.char == ">",
+                         "media", "message")
 
   # handling of techuser
   if(techuser == FALSE){
